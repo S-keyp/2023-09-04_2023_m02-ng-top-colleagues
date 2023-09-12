f15a68aa81bc827d878710ce1f2ae9ab32a64866
S-Keyp <s-keyp@outlook.fr>
2023-09-12 14:44:41 +0200

    fix: refresh for colleague

--- src/app/providers/colleague.service.ts
+++ src/app/providers/colleague.service.ts
@@ -17,7 +17,6 @@ export class ColleagueService {
 	constructor(private http: HttpClient) { }
 
 	getCollegueByPseudo(pseudo:string){
-		
 		return this.http.get<Colleague>('https://app-6f6e9c23-7f63-4d86-975b-a0b1a1440f94.cleverapps.io/api/v2/colleagues/'+pseudo).pipe(
 			catchError((error) => {
 				return of(null);
